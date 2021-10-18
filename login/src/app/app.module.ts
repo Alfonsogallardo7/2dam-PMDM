abb2c43afaa17d4eff54b478b391f40873468a52
Alfonso Gallardo Rodríguez <gallardo.roalf20@triana.salesianos.edu>
2021-10-18 12:38:48 +0200

    Añadiendo la actividad UD02-04, no está terminada entera por que me tuve que ir al médico

---
+++ login/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MaterialImportsModule } from './material-imports/material-imports.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormFieldComponent } from './form-field/form-field.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    FormFieldComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialImportsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
